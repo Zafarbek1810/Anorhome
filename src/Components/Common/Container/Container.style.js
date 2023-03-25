d04222bef14c9c03f9e2e925b0542e95b49357a8
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2023-03-25 11:25:37 +0500

    container change

--- src/Components/Common/Container/Container.style.js
+++ src/Components/Common/Container/Container.style.js
@@ -7,6 +7,10 @@ const StyledContainer = styled.div`
   margin-right: auto;
   padding-left: 15px;
   padding-right: 15px;
+
+  @media (max-width: 700px) {
+    max-width: ${(props) => (props.width ? `${props.width}px` : "95vw")};
+  }
 `;
 
 export { StyledContainer };
