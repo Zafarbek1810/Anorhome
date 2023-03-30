1880c94697e1958e193f834879a877bc55012bcb
Erkinovv <davlatyorwalker@gmail.com>
2023-03-30 15:25:46 +0500

    add live chat

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import '../styles/globals.css'
 
   
 import "aos/dist/aos.css";
+import MyTawk from '../src/Components/Common/Tawk';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <HeaderProvider>
     <Component {...pageProps} />
+    <MyTawk />
     </HeaderProvider>
 
   )
