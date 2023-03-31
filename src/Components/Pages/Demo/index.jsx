45c03b7b11cc22f18d8c1759cecd46cb1fb6ad11
Zafarbek1810 <zafarbekyoldoshev1810@gmail.com>
2023-03-31 11:42:04 +0500

    fix bug

--- src/Components/Pages/Demo/index.jsx
+++ src/Components/Pages/Demo/index.jsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
-import MyLink from "../../Common/MyLink";
+import React from "react";
 import { DemoStyle } from "./DemoStyle";
-import Container from "../../Common/Container";
-import { Carousel } from "antd";
+
 
 const Demo = () => {
-  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
-
-  function handleCardClick(index) {
-    setSelectedCardIndex(index);
-  }
+  
   return (
     <DemoStyle>
-      <div className="card-information">
-        <div className="card-list">
-          {props.cards.map((card, index) => (
-            <div
-              key={index}
-              className={`card ${
-                index === selectedCardIndex ? "selected" : ""
-              }`}
-              onClick={() => handleCardClick(index)}
-            >
-              <h3>{card.title}</h3>
-              <p>{card.description}</p>
-            </div>
-          ))}
-        </div>
-        <div className="card-info">
-          {selectedCardIndex !== null ? (
-            <div>
-              <h2>{props.cards[selectedCardIndex].title}</h2>
-              <p>{props.cards[selectedCardIndex].info}</p>
-            </div>
-          ) : (
-            <p>Select a card to view its information</p>
-          )}
-        </div>
-      </div>
+      demo
     </DemoStyle>
   );
 };
