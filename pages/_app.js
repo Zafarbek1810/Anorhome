a4017d85db4a6dcea6980a07507e6415b2b17cb4
Erkinovv <davlatyorwalker@gmail.com>
2023-03-31 10:49:00 +0500

    add chatra

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { HeaderProvider } from '../src/Context/HeaderContext'
 import '../styles/globals.css'
 
@@ -7,10 +8,25 @@ import MyTawk from '../src/Components/Common/Tawk';
 
 
 function MyApp({ Component, pageProps }) {
+ 
+
+  useEffect(()=>{
+    (function(d, w, c) {
+      w.ChatraID = 'PBvLdNYPuo2dux3ib';
+      var s = d.createElement('script');
+      w[c] = w[c] || function() {
+          (w[c].q = w[c].q || []).push(arguments);
+      };
+      s.async = true;
+      s.src = 'https://call.chatra.io/chatra.js';
+      if (d.head) d.head.appendChild(s);
+  })(document, window, 'Chatra');
+},[])
+
   return (
     <HeaderProvider>
     <Component {...pageProps} />
-    <MyTawk />
+    {/* <MyTawk /> */}
     </HeaderProvider>
 
   )
