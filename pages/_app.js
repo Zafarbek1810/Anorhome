e1ae1142728b320c3934efe65665a578d2addb1f
Erkinovv <davlatyorwalker@gmail.com>
2023-03-31 15:36:33 +0500

    add jivochat

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
-import { useEffect } from 'react';
 import { HeaderProvider } from '../src/Context/HeaderContext'
 import '../styles/globals.css'
 import Modal from "../src/Components/Common/ContactModal"
 import FormModal from "../src/Components/Common/ContactModal/FormModal"
 import "aos/dist/aos.css";
 import { ModalProvider } from '../src/Context/ModalContext/Context'; 
+import Script from 'next/script'
 
 
-function MyApp({ Component, pageProps }) {
-  
+function MyApp({ Component, pageProps }) { 
 
   return (
     <HeaderProvider>
@@ -18,6 +17,7 @@ function MyApp({ Component, pageProps }) {
           <FormModal />
         </Modal>
       </ModalProvider>
+          <Script src="//code.jivosite.com/widget/guz7xA8JEU" async /> 
     </HeaderProvider>
 
   )
